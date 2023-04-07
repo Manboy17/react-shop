b92519e8f35e03bf4e816424357a018016c45a6c
Hlushchenko Denys <104257015+Manboy17@users.noreply.github.com>
2023-04-07 21:30:33 +0200

    Update Routers.js

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -10,8 +10,7 @@ import Signup from "../pages/Signup";
 const Routers = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/home" />} />
-      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/shop" element={<Shop />} />
       <Route path="/shop/:id" element={<ProductDetails />} />
       <Route path="/cart" element={<Cart />} />
