85e71500886f470a32b370f9d8dd3070e30d1f58
Hlushchenko Denys <104257015+Manboy17@users.noreply.github.com>
2023-04-07 21:31:31 +0200

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,9 +4,6 @@ import { Link, useNavigate } from "react-router-dom";
 
 import "../styles/Login.css";
 import { useState } from "react";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase.config";
-import { toast } from "react-toastify";
 
 const Login = () => {
   const [email, setEmail] = useState("");
